2477414a80f075d8379a9616a8d60051034d1326
adisos <adisos@il.ibm.com>
2023-04-24 16:17:29 +0300

    add md output
    Signed-off-by: adisos <adisos@il.ibm.com>

---
+++ pkg/vpcmodel/mdOutput.go
@@ -0,0 +1,34 @@
+package vpcmodel
+
+import (
+	"fmt"
+	"strings"
+)
+
+type MDoutputFormatter struct {
+}
+
+const mdHeader = "| src | dst | conn |\n|-----|-----|------|"
+
+// formats a connection line for md output
+func getMDLine(c connLine) string {
+	return fmt.Sprintf("| %s | %s | %s |", c.Src.Name(), c.Dst.Name(), c.Conn)
+}
+
+func (m *MDoutputFormatter) WriteOutput(c *CloudConfig, conn *VPCConnectivity, outFile string) (string, error) {
+	lines := []string{mdHeader}
+
+	for src, srcMap := range conn.AllowedConnsCombined {
+		for dst, conn := range srcMap {
+			if conn.IsEmpty() {
+				continue
+			}
+			connLineObj := connLine{Src: src, Dst: dst, Conn: conn.String()}
+			lines = append(lines, getMDLine(connLineObj))
+		}
+	}
+
+	out := strings.Join(lines, "\n")
+	err := writeToFile(out, outFile)
+	return out, err
+}
