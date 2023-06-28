adcce7057227bb2d9f2253063888bc040e3ecb03
haim-kermany <haim.kermany@gmail.com>
2023-06-28 11:24:05 +0300

    documenting

--- pkg/vpcmodel/drawioOutput.go
+++ pkg/vpcmodel/drawioOutput.go
@@ -14,26 +14,36 @@ type Edge struct {
 
 // DrawioOutputFormatter create the drawio connectivity map.
 // It build the drawio tree out of the CloudConfig and VPCConnectivity, and output it to a drawio file
+// the steps of creating the drawio tree:
+// 1. collect all the connectivity edges to a map of (src,dst,label) -> isDirected. also mark the nodes that has connections
+// 2. create the treeNodes of the squares out of the cConfig.NodeSets -  network, VPCs zones and subnets
+// (also zones are created on demand from the zone names in the nodeSet details)
+// 3. from the cConfig.filters,  create treeNodes of SGs, and give ACL to subnets
+// 4. create the icons Tree nodes out of the cConfig.Nodes
+// 5. create the VSIs tree nodes from cConfig.NodeSets
+// 6. create the routers from cConfig.routers
+// 7. create the edges from the map we created in stage (1). also also set the routers to the edges
+
 type DrawioOutputFormatter struct {
-	cConfig                *CloudConfig
-	conn                   *VPCConnectivity
-	network                *drawio.NetworkTreeNode
-	vpc                    *drawio.VpcTreeNode
-	allZonesTreeNodes      map[string]*drawio.ZoneTreeNode
-	uidToSubnetsTreeNodes  map[string]*drawio.SubnetTreeNode
-	cidrToSubnetsTreeNodes map[string]*drawio.SubnetTreeNode
-	allIconsTreeNodes      map[NamedResourceIntf]drawio.IconTreeNodeInterface
-	isExternalIcon         map[drawio.IconTreeNodeInterface]bool
-	connectedNodes         map[NamedResourceIntf]bool
-	routers                map[drawio.IconTreeNodeInterface]drawio.IconTreeNodeInterface
-	sgMembers              map[string]*drawio.SGTreeNode
-	isEdgeDirected         map[Edge]bool
+	cConfig                  *CloudConfig
+	conn                     *VPCConnectivity
+	network                  *drawio.NetworkTreeNode
+	vpc                      *drawio.VpcTreeNode
+	zoneNameToZonesTreeNodes map[string]*drawio.ZoneTreeNode
+	uidToSubnetsTreeNodes    map[string]*drawio.SubnetTreeNode
+	cidrToSubnetsTreeNodes   map[string]*drawio.SubnetTreeNode
+	allIconsTreeNodes        map[NamedResourceIntf]drawio.IconTreeNodeInterface
+	isExternalIcon           map[drawio.IconTreeNodeInterface]bool
+	connectedNodes           map[NamedResourceIntf]bool
+	routers                  map[drawio.IconTreeNodeInterface]drawio.IconTreeNodeInterface
+	sgMembers                map[string]*drawio.SGTreeNode
+	isEdgeDirected           map[Edge]bool
 }
 
 func (d *DrawioOutputFormatter) init(cConfig *CloudConfig, conn *VPCConnectivity) {
 	d.cConfig = cConfig
 	d.conn = conn
-	d.allZonesTreeNodes = map[string]*drawio.ZoneTreeNode{}
+	d.zoneNameToZonesTreeNodes = map[string]*drawio.ZoneTreeNode{}
 	d.uidToSubnetsTreeNodes = map[string]*drawio.SubnetTreeNode{}
 	d.cidrToSubnetsTreeNodes = map[string]*drawio.SubnetTreeNode{}
 	d.allIconsTreeNodes = map[NamedResourceIntf]drawio.IconTreeNodeInterface{}
@@ -63,10 +73,10 @@ func (d *DrawioOutputFormatter) createDrawioTree() {
 
 func (d *DrawioOutputFormatter) getZoneTreeNode(resource NamedResourceIntf) *drawio.ZoneTreeNode {
 	zoneName := resource.(ZonalNamedResourceIntf).ZoneName()
-	if _, ok := d.allZonesTreeNodes[zoneName]; !ok {
-		d.allZonesTreeNodes[zoneName] = drawio.NewZoneTreeNode(d.vpc, zoneName)
+	if _, ok := d.zoneNameToZonesTreeNodes[zoneName]; !ok {
+		d.zoneNameToZonesTreeNodes[zoneName] = drawio.NewZoneTreeNode(d.vpc, zoneName)
 	}
-	return d.allZonesTreeNodes[zoneName]
+	return d.zoneNameToZonesTreeNodes[zoneName]
 }
 
 func (d *DrawioOutputFormatter) createEdgesMap() {
@@ -141,20 +151,6 @@ func (d *DrawioOutputFormatter) createNodes() {
 			if d.connectedNodes[n] {
 				d.allIconsTreeNodes[n] = drawio.NewInternetTreeNode(d.network, details[DetailsAttributeCIDR])
 				d.isExternalIcon[d.allIconsTreeNodes[n]] = true
-			}
-		}
-	}
-	for _, ns := range d.cConfig.NodeSets {
-		details := ns.DetailsMap()
-		if details[DetailsAttributeKind] == "VSI" {
-			if len(ns.Nodes()) == 0 {
-				continue
-			} else {
-				vsiNIs := []drawio.TreeNodeInterface{}
-				for _, ni := range ns.Nodes() {
-					vsiNIs = append(vsiNIs, d.allIconsTreeNodes[ni])
-				}
-				drawio.GroupNIsWithVSI(d.getZoneTreeNode(ns), ns.Name(), vsiNIs)
 			}
 		}
 	}
