61cafed2972d8dbd2358462d4cf77994e3760f02
shirim <shirim@il.ibm.com>
2023-09-20 16:02:58 +0300

    added unit test to PR https://github.com/np-guard/vpc-network-config-analyzer/pull/132 of grouped external address presented as range

--- pkg/vpcmodel/grouping_test.go
+++ pkg/vpcmodel/grouping_test.go
@@ -160,7 +160,8 @@ func newCloudConfigTest7() (*CloudConfig, *VPCConnectivity) {
 	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[0], res.Nodes[1], common.NewConnectionSetWithStateful(true, common.StatefulTrue))
 	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[0], res.Nodes[2], common.NewConnectionSetWithStateful(true, common.StatefulTrue))
 	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[3], res.Nodes[1], common.NewConnectionSetWithStateful(true, common.StatefulTrue))
-	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[3], res.Nodes[2], common.NewConnectionSetWithStateful(true, common.StatefulFalse))
+	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[3], res.Nodes[2],
+		common.NewConnectionSetWithStateful(true, common.StatefulFalse))
 
 	return res, res1
 }
@@ -174,6 +175,34 @@ func TestGroupingPhase7(t *testing.T) {
 	require.Equal(t, "vsi1 => Public Internet 1.2.0.0/22,8.8.8.8/32 : All Connections\n"+
 		"vsi2 => Public Internet 1.2.0.0/22 : All Connections\n"+
 		"vsi2 => Public Internet 8.8.8.8/32 : All Connections *\n\n"+
+		"connections are stateful unless marked with *\n", groupingStr)
+	fmt.Println(groupingStr)
+	fmt.Println("done")
+}
+
+// grouping that results in cidrs presented as range and not as cidr
+func newCloudConfigTest8() (*CloudConfig, *VPCConnectivity) {
+	res := &CloudConfig{Nodes: []Node{}}
+	res.Nodes = append(res.Nodes,
+		&mockNetIntf{cidr: "10.0.20.5/32", name: "vsi1"},
+		&mockNetIntf{cidr: "1.2.3.0/24", name: "public1", isPublic: true},
+		&mockNetIntf{cidr: "1.2.4.0/24", name: "public2", isPublic: true})
+
+	res.NodeSets = append(res.NodeSets, &mockSubnet{"10.0.20.0/22", "subnet1", []Node{res.Nodes[0]}})
+
+	res1 := &VPCConnectivity{AllowedConnsCombined: NewNodesConnectionsMap()}
+	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[0], res.Nodes[1], common.NewConnectionSet(true))
+	res1.AllowedConnsCombined.updateAllowedConnsMap(res.Nodes[0], res.Nodes[2], common.NewConnectionSet(true))
+	return res, res1
+}
+
+func TestGroupingPhase8(t *testing.T) {
+	c, v := newCloudConfigTest8()
+	res := &GroupConnLines{c: c, v: v, srcToDst: newGroupingConnections(), dstToSrc: newGroupingConnections()}
+	res.groupExternalAddresses()
+	res.groupInternalSrcOrDst(true, true)
+	groupingStr := res.String()
+	require.Equal(t, "vsi1 => Public Internet 1.2.3.0-1.2.4.255 : All Connections\n\n"+
 		"connections are stateful unless marked with *\n", groupingStr)
 	fmt.Println(groupingStr)
 	fmt.Println("done")
