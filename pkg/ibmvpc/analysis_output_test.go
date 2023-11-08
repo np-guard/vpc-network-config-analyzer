be9d66978aeecc354e7efaa2487ae71552ec855c
ShiriMoran <139739065+ShiriMoran@users.noreply.github.com>
2023-11-08 15:29:06 +0200

    Update pkg/ibmvpc/analysis_output_test.go
    Co-authored-by: Adi Sosnovich <82078442+adisos@users.noreply.github.com>

--- pkg/ibmvpc/analysis_output_test.go
+++ pkg/ibmvpc/analysis_output_test.go
@@ -337,6 +337,8 @@ var tests = []*vpcGeneralTest{
 	},
 	{
 		name:     "acl_testing5",
+                // TODO: currently for this test, there are 2 connections that only differ in statefulness attribute, and 
+                // are not yet displayed in the diff report (sub1-1-ky => sub1-2-ky , sub1-1-ky => sub1-3-ky)
 		useCases: []vpcmodel.OutputUseCase{vpcmodel.AllSubnetsDiff},
 		format:   vpcmodel.Text,
 	},
