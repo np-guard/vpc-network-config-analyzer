09f2da477368ca0da5ed98d1e28d98b16c084d30
shirim <shirim@il.ibm.com>
2023-11-09 11:45:50 +0200

    adjusted end-to-end test

--- pkg/ibmvpc/analysis_output_test.go
+++ pkg/ibmvpc/analysis_output_test.go
@@ -335,13 +335,13 @@ var tests = []*vpcGeneralTest{
 		useCases: []vpcmodel.OutputUseCase{vpcmodel.AllSubnets},
 		format:   vpcmodel.Text,
 	},
-	//{
-	//	name: "acl_testing5",
-	//	// TODO: currently for this test, there are 2 connections that only differ in statefulness attribute, and
-	//	// are not yet displayed in the diff report (sub1-1-ky => sub1-2-ky , sub1-1-ky => sub1-3-ky)
-	//	useCases: []vpcmodel.OutputUseCase{vpcmodel.AllSubnetsDiff},
-	//	format:   vpcmodel.Text,
-	//},
+	{
+		name: "acl_testing5",
+		// TODO: currently for this test, there are 2 connections that only differ in statefulness attribute, and
+		// are not yet displayed in the diff report (sub1-1-ky => sub1-2-ky , sub1-1-ky => sub1-3-ky)
+		useCases: []vpcmodel.OutputUseCase{vpcmodel.AllSubnetsDiff},
+		format:   vpcmodel.Text,
+	},
 }
 
 var formatsAvoidComparison = map[vpcmodel.OutFormat]bool{vpcmodel.ARCHDRAWIO: true, vpcmodel.DRAWIO: true}
