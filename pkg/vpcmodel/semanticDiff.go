268a3e8200a1fdb5a4843488cf800ad200e8e2fd
shirim <shirim@il.ibm.com>
2023-11-19 09:49:23 +0200

    test after git issue

--- pkg/vpcmodel/semanticDiff.go
+++ pkg/vpcmodel/semanticDiff.go
@@ -62,6 +62,7 @@ type diffBetweenCfgs struct {
 	groupedLines []*GroupedConnLine
 }
 
+// tmp git test
 func (configs ConfigsForDiff) GetDiff() (*diffBetweenCfgs, error) {
 	// 1. compute connectivity for each of the configurations
 	var generalConnectivityMap1, generalConnectivityMap2 generalConnectivityMap
