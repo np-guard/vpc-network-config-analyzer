466567fd30d2a879eb0a995ba6693331313cd552
shirim <shirim@il.ibm.com>
2023-12-04 12:16:56 +0200

    CR comment: move GeneralConnectivityMap to commonConnectivity.go

--- pkg/vpcmodel/commonConnectivity.go
+++ pkg/vpcmodel/commonConnectivity.go
@@ -2,6 +2,8 @@ package vpcmodel
 
 import "github.com/np-guard/vpc-network-config-analyzer/pkg/common"
 
+type GeneralConnectivityMap map[VPCResourceIntf]map[VPCResourceIntf]*common.ConnectionSet
+
 func (connectivityMap GeneralConnectivityMap) updateAllowedConnsMap(src, dst VPCResourceIntf, conn *common.ConnectionSet) {
 	if _, ok := connectivityMap[src]; !ok {
 		connectivityMap[src] = map[VPCResourceIntf]*common.ConnectionSet{}
