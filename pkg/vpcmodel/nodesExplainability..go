27a52a624acb1da8da137e7d59cc169933fc6c4e
shirim <shirim@il.ibm.com>
2023-12-07 16:02:54 +0200

    updated general flow after talking with Adi and also with Ziv

--- pkg/vpcmodel/nodesExplainability..go
+++ pkg/vpcmodel/nodesExplainability..go
@@ -1,6 +1,6 @@
 package vpcmodel
 
-// stage 1: vsi to vsi of the same subnet for which a connection exist
+// stage 1: vsi to vsi of the same subnet (Adi prefers the definition "vsi to vsi considering only SGs")
 //          this is relatively simple since only SG can effect this connection, and SG has only enabling rules
 
 // stage 1:
@@ -9,14 +9,23 @@ package vpcmodel
 // 2. Find the ingress and egress rules enabling the connection in the SGAnalyzer struct.
 //    specifically, the []*SGRule ingressRules and egressRules contributing to the connection.
 //    There are two alternatives of achieving this:
-//    * adding the following functionality:
+//    a) adding the following functionality:
 //      func (sg *SecurityGroup) ConnOfSGRules(src, dst vpcmodel.Node, isIngress bool) []*SGRule/[]uniqueIdsOfSGRules
 //      whose functionality is similar to that of
 //              func (sg *SecurityGroup) AllowedConnectivity(src, dst vpcmodel.Node, isIngress bool) *common.ConnectionSet
 //      and for the most part based on the same code
-//    * Enhancing the structure
+//    In this case after ParseResourcesFromFile and VPCConfigsFromResources dedicated analysis based on ConnOfSGRules will be performed,
+//    and the general connectivity analysis functionality will not be called
+//
+//    b) Enhancing the structure
 //        type ConnectivityResult struct {
 //       by adding to it the list of the rules contributing to the connection []*SGRule/[]uniqueIdsOfSGRules
+//    In this case in addition to ParseResourcesFromFile and VPCConfigsFromResources, GetVPCNetworkConnectivity will be called
+//     after which a dedicated function for collecting the relevant SG rules will be deployed.
+//
 //  3. Extract the actual sg rules from *SGRule and print as output.
 //
 //     * pointer to the original rule has to be added to SGRule struct [perhaps the first PR here]
+
+// ToDo:
+// Highlevel of handling nacl as lack of connection explanability as well, just to see we will not need to rewrite everything
