a972258bb1dd945c24218a359a7ae05d765643ef
Ola Saadi <ola.saadi1@ibm.com>
2023-12-18 10:36:42 +0200

    fix version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,4 +7,4 @@ SPDX-License-Identifier: Apache-2.0
 package version
 
 // versionCore is the core portion of the cloud-resource-collector CLI version per Semantic Versioning 2.0.0
-const VersionCore = "0.1"
+const VersionCore = "0.2.1"
