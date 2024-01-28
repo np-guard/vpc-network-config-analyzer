72f967a867a766f41b42f4177a574ec31875cb45
shirim <shirim@il.ibm.com>
2024-01-28 16:41:46 +0200

    test new format updating 4

--- pkg/ibmvpc/explainability_test.go
+++ pkg/ibmvpc/explainability_test.go
@@ -190,7 +190,6 @@ var explainTests = []*explainGeneralTest{
 			"~~~~~~~~~~~~~\nSecurityGroupLayer Rules\n------------------------\nenabling rules from sg2-ky:\n\t" +
 			"index: 3, direction: outbound,  conns: protocol: icmp,  icmpType: protocol: ICMP, cidr: 142.0.0.0/8\n\n",
 	},*/
-	// todo: first test in QueryConnectionSGBasic is missing!!!
 	{
 		name:        "QueryConnectionSGBasic1",
 		inputConfig: "input_sg_testing1_new",
@@ -358,6 +357,33 @@ var explainTests = []*explainGeneralTest{
 			"\tindex: 1, direction: inbound , src: 0.0.0.0/0 , dst: 0.0.0.0/0, conn: all, action: allow\nSecurityGroupLayer Rules\n" +
 			"------------------------\nenabling rules from sg1-ky:\n\tindex: 4, direction: inbound, " +
 			"protocol: all, cidr: 10.240.30.5/32,10.240.30.6/32\n\n",
+	},
+	{
+		name:        "NACLExternal1",
+		inputConfig: "input_acl_testing3",
+		ESrc:        "vsi1-ky[10.240.10.4]",
+		EDst:        "161.26.0.0/16",
+		out: "The following connection exists between vsi1-ky[10.240.10.4] and Public Internet 161.26.0.0/16: protocol: UDP; " +
+			"its enabled by\nExternal Router PublicGateway: public-gw-ky\nEgress Rules:\n~~~~~~~~~~~~~\nNaclLayer Rules\n------------------------\n" +
+			"enabling rules from acl1-ky:\n\tindex: 1, direction: outbound , src: 10.240.10.0/24 , dst: 161.26.0.0/16, conn: protocol: udp, " +
+			"srcPorts: 1-65535, dstPorts: 1-65535, action: allow\nSecurityGroupLayer Rules\n------------------------\nenabling rules from sg1-ky:\n" +
+			"\tindex: 0, direction: outbound, protocol: all, cidr: 0.0.0.0/0\n\n",
+	},
+	{
+		name:        "NACLExternal2",
+		inputConfig: "input_acl_testing3",
+		ESrc:        "vsi1-ky[10.240.10.4]",
+		EDst:        "100.128.0.0/32",
+		out: "No connection between vsi1-ky[10.240.10.4] and Public Internet 100.128.0.0/32;" +
+			" connection blocked by egress\n\n",
+	},
+	{
+		name:        "NACLExternal3",
+		inputConfig: "input_acl_testing3",
+		ESrc:        "100.128.0.0/32",
+		EDst:        "vsi1-ky[10.240.10.4]",
+		out: "No connection between Public Internet 100.128.0.0/32 and vsi1-ky[10.240.10.4]; no fip router and src is external" +
+			" (fip is required for outbound external connection)\n\n",
 	},
 }
 
