fc01554e6a3c65b769416230fcd373abd00a6438
Ola Saadi <ola.saadi1@ibm.com>
2024-01-30 11:28:34 +0200

    add test

--- cmd/analyzer/main_test.go
+++ cmd/analyzer/main_test.go
@@ -21,6 +21,7 @@ func TestMain(t *testing.T) {
 		// {"json_diff_acl_testing5", "-output-file acl_testing5_diff.json -vpc-config ../../pkg/ibmvpc/examples/input_acl_testing5.json -vpc-config-second ../../pkg/ibmvpc/examples/input_acl_testing5_2nd.json -format json -analysis-type diff_all_subnets"},
 		{"txt_diff_acl_testing5", "-output-file acl_testing5_diff.txt -vpc-config ../../pkg/ibmvpc/examples/input_acl_testing5.json -vpc-config-second ../../pkg/ibmvpc/examples/input_acl_testing5_2nd.json -format txt -analysis-type diff_all_subnets"},
 		{"txt_diff_acl_testing3", "-output-file acl_testing3_diff.txt -vpc-config ../../pkg/ibmvpc/examples/input_acl_testing3.json -vpc-config-second ../../pkg/ibmvpc/examples/input_acl_testing3_2nd.json -format txt -analysis-type diff_all_endpoints"},
+		{"txt_all_subnets_342", "-output-file 342_all_subnets.txt -vpc-config ../../pkg/ibmvpc/examples/input_obj_from_issue_342.json -format txt -analysis-type all_subnets"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
