c0333456e1948676b4986ac5ef0731ed131134a4
shirim <shirim@il.ibm.com>
2024-02-04 12:52:23 +0200

    removing dup test.

--- pkg/ibmvpc/analysis_output_test.go
+++ pkg/ibmvpc/analysis_output_test.go
@@ -203,23 +203,6 @@ var tests = []*vpcGeneralTest{
 		useCases: []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints, vpcmodel.SingleSubnet},
 		format:   vpcmodel.Text,
 	},
-	//{
-	//	name:     "acl_testing3",
-	//	useCases: []vpcmodel.OutputUseCase{vpcmodel.Explain},
-	//	format:   vpcmodel.Text,
-	//	ESrc:     "vsi1-ky[10.240.10.4]",
-	//	EDst:     "vsi2-ky[10.240.20.4]",
-	//},
-	{
-		name:        "sg_testing1_new",
-		useCases:    []vpcmodel.OutputUseCase{vpcmodel.Explain},
-		format:      vpcmodel.Text,
-		ESrc:        "vsi1-ky[10.240.10.4]",
-		EDst:        "vsi2-ky[10.240.20.4]",
-		EProtocol:   "TCP",
-		ESrcMinPort: 1,
-		ESrcMaxPort: 5,
-	},
 	{
 		name:     "sg_testing1_new",
 		useCases: []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints, vpcmodel.SingleSubnet, vpcmodel.AllSubnets},
