f2786119a239cfdaf977c71d3796c046a8034b61
ShiriMoran <139739065+ShiriMoran@users.noreply.github.com>
2024-02-18 14:29:50 +0200

    Update cmd/analyzer/parse_args.go
    Co-authored-by: Adi Sosnovich <82078442+adisos@users.noreply.github.com>

--- cmd/analyzer/parse_args.go
+++ cmd/analyzer/parse_args.go
@@ -188,7 +188,7 @@ func ParseInArgs(cmdlineArgs []string) (*InArgs, error) {
 	args.Version = flagset.Bool(Version, false, "Prints the release version number")
 	args.ESrc = flagset.String(ESrc, "", "Source endpoint for explanation; "+
 		"can be specified as a VSI name/CRN or an internal/external IP-address/CIDR")
-	args.EDst = flagset.String(EDst, "", "Destination name for explanation; same format as src")
+	args.EDst = flagset.String(EDst, "", "Destination endpoint for explanation; same format as src")
 	args.EProtocol = flagset.String(EProtocol, "", "Protocol for connection description")
 	args.ESrcMinPort = flagset.Int64(ESrcMinPort, common.MinPort, "Minimum source port for connection description")
 	args.ESrcMaxPort = flagset.Int64(ESrcMaxPort, common.MaxPort, "Maximum source port for connection description")
