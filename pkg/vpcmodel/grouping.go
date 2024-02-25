51eac607f20482dab0bb9e8d6cd32b68dfbc6ca6
shirim <shirim@il.ibm.com>
2024-02-25 17:48:48 +0200

    restoring code to its orig place to ease CR

--- pkg/vpcmodel/grouping.go
+++ pkg/vpcmodel/grouping.go
@@ -185,6 +185,30 @@ func (g *groupedExternalNodes) Name() string {
 	return prefix + g.String()
 }
 
+// given a groupedEndpointsElems returns an equiv item from groupedEndpointsElemsMap if exists,
+// or adds it to groupedEndpointsElemsMap if such an item does not exist
+func getGroupedEndpointsElems(grouped groupedEndpointsElems,
+	groupedEndpointsElemsMap map[string]*groupedEndpointsElems) *groupedEndpointsElems {
+	// since the endpoints (vsis/subnets) are sorted before printed, grouped.Name() will be identical
+	// to equiv groupedEndpointsElems
+	if existingGrouped, ok := groupedEndpointsElemsMap[grouped.Name()]; ok {
+		return existingGrouped
+	}
+	groupedEndpointsElemsMap[grouped.Name()] = &grouped
+	return &grouped
+}
+
+// same as the previous function, for groupedExternalNodesMap
+func getGroupedExternalNodes(grouped groupedExternalNodes,
+	groupedExternalNodesMap map[string]*groupedExternalNodes) *groupedExternalNodes {
+	// Due to the canonical representation, grouped.String() and thus grouped.Name() will be identical
+	//  to equiv groupedExternalNodes
+	if existingGrouped, ok := groupedExternalNodesMap[grouped.Name()]; ok {
+		return existingGrouped
+	}
+	groupedExternalNodesMap[grouped.Name()] = &grouped
+	return &grouped
+}
 func (g *groupingConnections) addPublicConnectivity(ep EndpointElem, commonProps *groupedCommonProperties, targetNode *ExternalNetwork) {
 	connKey := commonProps.groupingStrKey
 	if _, ok := (*g)[ep]; !ok {
@@ -416,6 +440,54 @@ func (g *GroupConnLines) groupInternalSrcOrDst(srcGrouping, groupVsi bool) {
 		}
 	}
 	g.GroupedLines = unifiedGroupedConnLines(res, g.groupedEndpointsElemsMap, nil, false)
+}
+
+// Go over the grouping result and set groups s.t. all semantically equiv groups have a unified reference.
+// this is required for multivpc's context and at the end of the grouping in a single vpc context
+// the former is required since each vpc analysis and grouping is done separately
+// the latter is required due to the functionality treating self loops as don't cares - extendGroupingSelfLoops
+// in which both srcs and dsts are manipulated  but *GroupConnLines is not familiar
+// within the extendGroupingSelfLoops context and thus can not be done there smoothly
+func unifiedGroupedConnLines(oldConnLines []*groupedConnLine,
+	groupedEndpointsElemsMap map[string]*groupedEndpointsElems,
+	groupedExternalNodesMap map[string]*groupedExternalNodes,
+	unifyGroupedExternalNodes bool) []*groupedConnLine {
+	newGroupedLines := make([]*groupedConnLine, len(oldConnLines))
+	// go over all connections; if src/dst is not external then use groupedEndpointsElemsMap
+	for i, groupedLine := range oldConnLines {
+		newGroupedLines[i] = &groupedConnLine{unifiedGroupedElems(groupedLine.src, groupedEndpointsElemsMap,
+			groupedExternalNodesMap, unifyGroupedExternalNodes),
+			unifiedGroupedElems(groupedLine.dst, groupedEndpointsElemsMap,
+				groupedExternalNodesMap, unifyGroupedExternalNodes),
+			groupedLine.commonProperties}
+	}
+	return newGroupedLines
+}
+
+// unifies reference to a single element
+func unifiedGroupedElems(srcOrDst EndpointElem,
+	groupedEndpointsElemsMap map[string]*groupedEndpointsElems,
+	groupedExternalNodesMap map[string]*groupedExternalNodes,
+	unifyGroupedExternalNodes bool) EndpointElem {
+	// external in case external grouping does not need to be unifed
+	if !unifyGroupedExternalNodes && srcOrDst.IsExternal() {
+		return srcOrDst
+	}
+	if _, ok := srcOrDst.(Node); ok { // single vsi or single node external address
+		return srcOrDst
+	}
+	if _, ok := srcOrDst.(NodeSet); ok { // single subnet
+		return srcOrDst
+	}
+	if groupedEE, ok := srcOrDst.(*groupedEndpointsElems); ok {
+		unifiedGroupedEE := getGroupedEndpointsElems(*groupedEE, groupedEndpointsElemsMap)
+		return unifiedGroupedEE
+	}
+	if groupedExternal, ok := srcOrDst.(*groupedExternalNodes); ok {
+		unifiedGroupedEE := getGroupedExternalNodes(*groupedExternal, groupedExternalNodesMap)
+		return unifiedGroupedEE
+	}
+	return srcOrDst
 }
 
 // computeGrouping does the grouping; for vsis (all_endpoints analysis)
