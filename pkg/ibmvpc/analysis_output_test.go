94ea40f6c8fd2472594ffd1bbd627d78d1d7008f
haim-kermany <haim.kermany@gmail.com>
2024-03-14 14:17:46 +0200

    clean code

--- pkg/ibmvpc/analysis_output_test.go
+++ pkg/ibmvpc/analysis_output_test.go
@@ -167,27 +167,6 @@ func (tt *vpcGeneralTest) initTest() {
 
 var tests = []*vpcGeneralTest{
 	{
-		inputConfig: "iks_config_object",
-		useCases:    []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints},
-		grouping:    false,
-		format:      vpcmodel.DRAWIO,
-	},
-	{
-		inputConfig: "load_balancer",
-		useCases:    []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints},
-		grouping:    false,
-		format:      vpcmodel.DRAWIO,
-	},
-	{
-		inputConfig: "load_balancer",
-		useCases:    []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints},
-		grouping:    false,
-		format:      vpcmodel.Text,
-	},
-}
-
-var tests2 = []*vpcGeneralTest{
-	{
 		inputConfig: "acl_testing5",
 		useCases:    []vpcmodel.OutputUseCase{vpcmodel.AllSubnets},
 		format:      vpcmodel.MD,
