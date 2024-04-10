900df72f737ba12108c6a39f1530808e8e303ca7
shirim <shirim@il.ibm.com>
2024-04-10 14:37:35 +0300

    src and dst found but not in the same cfg - missing cross vpc router. add tests as well

--- pkg/vpcmodel/explainabilityConnectivity.go
+++ pkg/vpcmodel/explainabilityConnectivity.go
@@ -74,6 +74,11 @@ func (configsMap MultipleVPCConfigs) ExplainConnectivity(src, dst string, connQu
 	if err != nil {
 		return nil, err
 	}
+	if vpcConfig == nil && err == nil {
+		// No error and also no matching vpc config for both src and dst: missing cross-vpc router.
+		// No VPCConfig to work with in this case, thus, this case is treated separately
+		return &Explanation{nil, connQuery, nil, src, dst, nil, nil, nil}, nil
+	}
 	return vpcConfig.explainConnectivityForVPC(src, dst, srcNodes, dstNodes, isSrcDstInternalIP, connQuery)
 }
 
