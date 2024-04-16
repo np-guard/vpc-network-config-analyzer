496f3a57c1cbdc9a2e9d9a2a45db4e986efe72e2
Ola Saadi <ola.saadi1@ibm.com>
2024-04-16 17:50:07 +0300

    add test

---
+++ pkg/ibmvpc/sg_analysis_test.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2024- IBM Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package ibmvpc
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/np-guard/cloud-resource-collector/pkg/ibm/datamodel"
+	"github.com/np-guard/vpc-network-config-analyzer/pkg/vpcmodel"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSGRule(t *testing.T) {
+	sgJSON := `{
+		"created_at": "2023-03-13T11:50:34Z",
+		"crn": "olaa",
+		"href": "href:9",
+		"id": "id:10",
+		"name": "sg1-ola",
+		"resource_group": {
+			"href": "href:6",
+			"id": "id:7",
+			"name": "anonymous"
+		},
+		"rules": [
+			{
+				"direction": "outbound",
+				"href": "href:151",
+				"id": "id:152",
+				"ip_version": "ipv4",
+				"protocol": "all",
+				"local": {
+					"cidr_block": "0.0.0.0/0"
+				},
+				"remote": {
+					"cidr_block": "0.0.0.0/0"
+				}
+			},
+			{
+				"direction": "inbound",
+				"href": "href:153",
+				"id": "id:154",
+				"ip_version": "ipv4",
+				"protocol": "all",
+				"local": {
+					"address": "10.240.10.0"
+				},
+				"remote": {
+					"cidr_block": "0.0.0.0/0"
+				}
+			}
+		],
+		"tags": [],
+		"targets": [
+			{
+				"href": "href:86",
+				"id": "id:87",
+				"name": "contest-dance-divided-brilliant",
+				"resource_type": "network_interface"
+			},
+			{
+				"href": "href:70",
+				"id": "id:71",
+				"name": "data-washstand-blot-scrambler",
+				"resource_type": "network_interface"
+			}
+		],
+		"vpc": {
+			"crn": "crn:12",
+			"href": "href:13",
+			"id": "id:14",
+			"name": "test-vpc2-ky",
+			"resource_type": "vpc"
+		}
+	}`
+
+	sg := datamodel.SecurityGroup{}
+	err := json.Unmarshal([]byte(sgJSON), &sg)
+	require.Nil(t, err)
+	sgResource := &SecurityGroup{
+		VPCResource: vpcmodel.VPCResource{
+			ResourceName: *sg.Name,
+			ResourceUID:  *sg.CRN,
+			ResourceType: ResourceTypeSG,
+			VPCRef:       nil,
+			Region:       "",
+		},
+		analyzer: NewSGAnalyzer(&sg.SecurityGroup), members: map[string]vpcmodel.Node{},
+	}
+	ruleStr, _, _, err := sgResource.analyzer.getSGRule(0)
+	require.Nil(t, err)
+	require.Equal(t, ruleStr, "index: 0, direction: outbound,  conns: protocol: all, remote: 0.0.0.0/0, local: 0.0.0.0/0\n")
+	ruleStr, _, _, err = sgResource.analyzer.getSGRule(1)
+	require.Nil(t, err)
+	require.Equal(t, ruleStr, "index: 1, direction: inbound,  conns: protocol: all, remote: 0.0.0.0/0, local: 10.240.10.0/32\n")
+}
