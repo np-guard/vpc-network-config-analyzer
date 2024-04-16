f88ce9aa098295d56148b7ea770e4246c37f4add
haim-kermany <haim.kermany@gmail.com>
2024-04-16 17:44:15 +0300

    lint

--- pkg/ibmvpc/parser.go
+++ pkg/ibmvpc/parser.go
@@ -69,7 +69,8 @@ func filterByVpcResourceGroupAndRegions(rc *datamodel.ResourcesContainerModel, v
 
 // VPCConfigsFromResources returns a map from VPC UID (string) to its corresponding VPCConfig object,
 // containing the parsed resources in the relevant model objects
-//nolint:funlen // its a serial of commands, no need to spill it
+//
+//nolint:funlen // serial list of commands, no need to spill it
 func VPCConfigsFromResources(rc *datamodel.ResourcesContainerModel, vpcID, resourceGroup string, regions []string, debug bool) (
 	vpcmodel.MultipleVPCConfigs, error) {
 	res := vpcmodel.MultipleVPCConfigs{}          // map from VPC UID to its config
