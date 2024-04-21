7d5b86a5093e252757bdcb86f06edfab9e63aa35
haim-kermany <haim.kermany@gmail.com>
2024-04-21 17:04:01 +0300

    fix explainability for private ips

--- pkg/vpcmodel/drawioOutput.go
+++ pkg/vpcmodel/drawioOutput.go
@@ -222,7 +222,8 @@ func (d *DrawioOutputFormatter) createExplanations() []drawio.ExplanationEntry {
 	explanationsInput := CreateMultiExplanationsInput(d.cConfigs, d.vpcConns, d.gConns)
 	// remove all the entries that are not shown on the canvas:
 	explanationsInput = slices.DeleteFunc(explanationsInput, func(e explainInputEntry) bool {
-		return !d.showResource(e.src) || !d.showResource(e.dst)
+		return !d.showResource(e.src) || !d.showResource(e.dst) ||
+		d.gen.TreeNode(e.src) == nil || d.gen.TreeNode(e.dst) == nil
 	})
 
 	explanations := MultiExplain(explanationsInput, d.vpcConns)
