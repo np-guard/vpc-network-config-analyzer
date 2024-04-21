f532e38a8c57d1413ba298f62247cb49b5ca2728
shirim <shirim@il.ibm.com>
2024-04-21 09:30:50 +0300

    fix err msg in test

--- pkg/ibmvpc/explainability_test.go
+++ pkg/ibmvpc/explainability_test.go
@@ -816,7 +816,7 @@ func TestMultiExplainabilityOutput(t *testing.T) {
 	require.Contains(t, outputString, "No connections are allowed from Public Internet (all ranges) to ky-vpc2-vsi[10.240.64.5];\n"+
 		"no fip and src is external (fip is required for outbound external connection)", "no connection external src entry")
 	require.Contains(t, outputString, "No connections are allowed from ky-vpc2-vsi[10.240.64.5] to Public Internet (all ranges);\n"+
-		"no fip/pgw and dst is external", "no connection external dst entry")
+		"\tThe dst is external but there is no Floating IP or Public Gateway connecting to public internet", "no connection external dst entry")
 	require.Contains(t, outputString, "ky-vpc1-vsi[10.240.0.5] -> security group ky-vpc1-sg -> ky-vpc1-net1 -> network ACL ky-vpc1-acl1 ->",
 		"connection vsi to vsi")
 	require.Contains(t, outputString, "ky-vpc1 -> TGW local-tg-ky -> ky-vpc2 ->", "connection vsi to vsi")
