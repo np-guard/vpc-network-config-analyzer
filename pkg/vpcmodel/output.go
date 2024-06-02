3a193d5467a493bd7208da1140d5455e5e1219bc
haim-kermany <haim.kermany@gmail.com>
2024-06-02 15:12:24 +0300

    fmt

--- pkg/vpcmodel/output.go
+++ pkg/vpcmodel/output.go
@@ -19,7 +19,7 @@ type OutFormat int64
 
 const overApproximationSign = "**"
 const statefulMessage = "\nconnections are stateful (on TCP) unless marked with *\n"
-const overApproximationMessage = "\nconnections marked with "+overApproximationSign+" are an over-approximation, not all private IPs have the same connectivity\n"
+const overApproximationMessage = "\nconnections marked with " + overApproximationSign + " are an over-approximation, not all private IPs have the same connectivity\n"
 
 const (
 	JSON OutFormat = iota
