d27b395b438ac39bf5aff1452f00ca49d599901a
shirim <shirim@il.ibm.com>
2024-06-03 09:45:18 +0300

    fixed spelling mistakes

--- pkg/vpcmodel/output.go
+++ pkg/vpcmodel/output.go
@@ -243,8 +243,8 @@ func WriteToFile(content, fileName string) (string, error) {
 }
 
 // getAsteriskDetails returns:
-// 1. The info message regarding non stateful conns  in the output, when relevant
-// 2. The info message regarding over-aproximated conns, when relevant
+// 1. The info message regarding non-stateful conns  in the output, when relevant
+// 2. The info message regarding over-approximated conns, when relevant
 func getAsteriskDetails(uc OutputUseCase, hasStatelessConn, hasOverApproximatedConn bool, outFormat OutFormat) string {
 	res := ""
 	if uc != SingleSubnet && (outFormat == Text || outFormat == MD || outFormat == Debug) {
