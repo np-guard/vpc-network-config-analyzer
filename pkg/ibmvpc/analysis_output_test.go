0d15a261ebbb1e0356d6c24118320b4a0413d3d5
haim-kermany <haim.kermany@gmail.com>
2024-06-09 18:51:36 +0300

    fix

--- pkg/ibmvpc/analysis_output_test.go
+++ pkg/ibmvpc/analysis_output_test.go
@@ -699,6 +699,7 @@ var formatsAvoidComparison = map[vpcmodel.OutFormat]bool{
 }
 
 // uncomment the function below to run for updating the expected output
+/*
 var formatsAvoidOutputGeneration = map[vpcmodel.OutFormat]bool{vpcmodel.ARCHDRAWIO: true, vpcmodel.DRAWIO: true}
 
 func TestAllWithGeneration(t *testing.T) {
@@ -719,6 +720,7 @@ func TestAllWithGeneration(t *testing.T) {
 	}
 	fmt.Println("done")
 }
+*/
 
 func TestAllWithComparison(t *testing.T) {
 	// tests is the list of tests to run
