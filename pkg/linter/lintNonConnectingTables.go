52d0640db71b80308306f580fa171d08efcb13ee
shirim <shirim@il.ibm.com>
2024-07-22 13:50:26 +0300

    renaming

--- pkg/linter/lintNonConnectingTables.go
+++ pkg/linter/lintNonConnectingTables.go
@@ -13,7 +13,7 @@ import (
 const nonConnectedTablesName = "non-connected tables"
 
 // nonConnectedTables: tables - sgs/nacls - not connected to any endpoint/subnet
-type nonConnectedTables struct {
+type nonConnectedTablesLint struct {
 	basicLinter
 }
 
@@ -25,22 +25,22 @@ type nonConnectedTable struct {
 // /////////////////////////////////////////////////////////
 // lint interface implementation for filterRuleSplitSubnetLint
 // ////////////////////////////////////////////////////////
-func (lint *nonConnectedTables) lintName() string {
+func (lint *nonConnectedTablesLint) lintName() string {
 	return nonConnectedTablesName
 }
 
-func (lint *nonConnectedTables) lintDescription() string {
-	return "Security tables not connected to any resource"
+func (lint *nonConnectedTablesLint) lintDescription() string {
+	return "Traffic controloing tables not connected to any resource"
 }
 
-func (lint *nonConnectedTables) check() error {
+func (lint *nonConnectedTablesLint) check() error {
 	for _, config := range lint.configs {
 		if config.IsMultipleVPCsConfig {
 			continue // no use in executing lint on dummy vpcs
 		}
 		for _, layer := range vpcmodel.FilterLayers {
-			_ = layer // todo tmp
-			//filterLayer := config.GetFilterTrafficResourceOfKind(layer)
+			filterLayer := config.GetFilterTrafficResourceOfKind(layer)
+			_, _ = layer, filterLayer // todo tmp
 			//rules, err := filterLayer.GetRules()
 			//if err != nil {
 			//	return err
