808d5ba9ea9088d8c00c81802dba0e5e17c050a7
Ola Saadi <ola.saadi1@ibm.com>
2024-07-22 13:39:28 +0300

    fix

--- pkg/commonvpc/parser.go
+++ pkg/commonvpc/parser.go
@@ -29,7 +29,6 @@ const (
 	// iksNodeResourceType is not actually used from input api objects, but is added by the parser to SGs with targets
 	// that should be added with iks nodes
 	IksNodeResourceType = "iks_node" // used as the type within api objects (e.g. SecurityGroup.Targets.ResourceType)
-	cidrSeparator       = ", "
 	lineSectionLen      = 41
 )
 
