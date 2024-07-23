ac6b398a98c661f8ea35431aff6765d1c76f6118
haim-kermany <haim.kermany@gmail.com>
2024-07-23 15:02:09 +0300

    CR

--- pkg/vpcmodel/explainabilityPrint.go
+++ pkg/vpcmodel/explainabilityPrint.go
@@ -115,7 +115,7 @@ func explainMissingCrossVpcRouter(src, dst string, connQuery *connection.Set) st
 // 2 is printed only when the connection is blocked. It is redundant when the entire path ("3") is printed. When
 // the connection is blocked and only part of the path is printed then 2 is printed so that the relevant information
 // is provided regardless of where the is blocking
-// 4 is printed only in detail mode
+// 4 is printed only in detailed mode
 func (g *groupedConnLine) explainabilityLineStr(c *VPCConfig, connQuery *connection.Set, allRulesDetails *rulesDetails,
 	verbose bool) string {
 	expDetails := g.CommonProperties.expDetails
