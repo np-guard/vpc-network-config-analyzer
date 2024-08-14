0891f1c86eecd398210426878a9adfefa2c83a72
haim-kermany <haim.kermany@gmail.com>
2024-08-14 10:31:16 +0300

    not fixing lint

--- pkg/commonvpc/parser.go
+++ pkg/commonvpc/parser.go
@@ -270,7 +270,7 @@ func NewSubnet(name, uid, zone, cidr string, vpc vpcmodel.VPCResourceIntf) (*Sub
 }
 
 func PrintLineSection() {
-	logging.Debugf("%s", strings.Repeat("-", lineSectionLen))
+	logging.Debugf(strings.Repeat("-", lineSectionLen))
 }
 
 func PrintSGRules(sg *SecurityGroup) {
@@ -281,7 +281,7 @@ func PrintSGRules(sg *SecurityGroup) {
 		for k := range sg.Members {
 			keys = append(keys, k)
 		}
-		logging.Debugf("members: %s", strings.Join(keys, ", "))
+		logging.Debugf("members: " + strings.Join(keys, ", "))
 	}
 	for i := 0; i < numRules; i++ {
 		strRule, _, _, err := sg.Analyzer.SgAnalyzer.GetSGRule(i)
@@ -291,7 +291,7 @@ func PrintSGRules(sg *SecurityGroup) {
 
 func PrintRule(ruleStr string, index int, err error) {
 	if err == nil {
-		logging.Debugf("%s", ruleStr)
+		logging.Debugf(ruleStr)
 	} else {
 		logging.Debugf("err for rule %d: %s\n", index, err.Error())
 	}
