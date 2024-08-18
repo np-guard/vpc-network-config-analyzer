077fc4ed56c48005e779e60605ad9fcddb1c329e
haim-kermany <haim.kermany@gmail.com>
2024-08-18 16:15:11 +0300

    comment out

--- pkg/awsvpc/analysis_output_test.go
+++ pkg/awsvpc/analysis_output_test.go
@@ -40,7 +40,7 @@ var tests = []*commonvpc.VpcGeneralTest{
 }
 
 // uncomment the function below to run for updating the expected output
-
+/*
 func TestAllWithGeneration(t *testing.T) {
 	// tests is the list of tests to run
 	for testIdx := range tests {
@@ -49,7 +49,7 @@ func TestAllWithGeneration(t *testing.T) {
 	}
 	fmt.Println("done")
 }
-
+*/
 func TestAllWithComparison(t *testing.T) {
 	// tests is the list of tests to run
 	for testIdx := range tests {
