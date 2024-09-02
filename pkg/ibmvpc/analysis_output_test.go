fc6b5d38a9400f4e24c2c602562fde36c1e2b03b
shirim <shirim@il.ibm.com>
2024-09-02 17:14:25 +0300

    uncomment failed ? test

--- pkg/ibmvpc/analysis_output_test.go
+++ pkg/ibmvpc/analysis_output_test.go
@@ -461,13 +461,13 @@ var tests = []*commonvpc.VpcGeneralTest{
 		Format:       vpcmodel.HTML,
 	},
 	// todo: before merge - this fails. fail seems not related to this branch. understand why and have fixed.
-	// {
-	//	InputConfig:  "iks_workers_large",
-	//	UseCases:     []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints},
-	//	Grouping:     true,
-	//	NoLbAbstract: true,
-	//	Format:       vpcmodel.DRAWIO,
-	// },
+	{
+		InputConfig:  "iks_workers_large",
+		UseCases:     []vpcmodel.OutputUseCase{vpcmodel.AllEndpoints},
+		Grouping:     true,
+		NoLbAbstract: true,
+		Format:       vpcmodel.DRAWIO,
+	},
 	// LB examples:
 	{
 		InputConfig: "lb_bad_practice",
