75a770c479366ffcedc753ac3a27c9de05ccf367
shirim <shirim@il.ibm.com>
2024-09-02 13:56:39 +0300

    added test in which blocking occurs only in private subnet - not working properly, yet

--- pkg/awsvpc/explainability_test.go
+++ pkg/awsvpc/explainability_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 var explainTests = []*commonvpc.VpcGeneralTest{
+	// existing connection between two endpoints
 	{
 		Name:          "ip_to_ip",
 		InputConfig:   "aws_sg_1",
@@ -23,6 +24,7 @@ var explainTests = []*commonvpc.VpcGeneralTest{
 		Format:        vpcmodel.Text,
 		DetailExplain: true,
 	},
+	// connection to the public internet blocked by sg and private subnet
 	{
 		Name:          "to_external_private_subnet",
 		InputConfig:   "aws_sg_1",
@@ -31,10 +33,20 @@ var explainTests = []*commonvpc.VpcGeneralTest{
 		Format:        vpcmodel.Text,
 		DetailExplain: true,
 	},
+	// existing connection to the public internet
 	{
 		Name:          "to_external_public_subnet",
 		InputConfig:   "aws_sg_1",
 		ESrc:          "10.240.10.42",
+		EDst:          "161.26.0.0",
+		Format:        vpcmodel.Text,
+		DetailExplain: true,
+	},
+	// connection to the public internet blocked only by private subnet
+	{
+		Name:          "to_external_blocked_only_private_subnet",
+		InputConfig:   "aws_sg_1_allow_sg_private",
+		ESrc:          "10.240.20.245",
 		EDst:          "161.26.0.0",
 		Format:        vpcmodel.Text,
 		DetailExplain: true,
@@ -55,7 +67,7 @@ func TestAll(t *testing.T) {
 }
 
 // uncomment the function below for generating the expected output files instead of comparing
-/*
+
 func TestAllWithGeneration(t *testing.T) {
 	// tests is the list of tests to run
 	for testIdx := range explainTests {
@@ -64,8 +76,8 @@ func TestAllWithGeneration(t *testing.T) {
 		t.Run(tt.Name, func(t *testing.T) {
 			t.Parallel()
 			rc := &AWSresourcesContainer{}
-			commonvpc.RunExplainTest(tt, t,rc)
+			commonvpc.RunExplainTest(tt, t, rc)
 		})
 	}
 	fmt.Println("done")
-}*/
+}
