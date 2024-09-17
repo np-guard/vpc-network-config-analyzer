f6e5878b1ab7d4fc85e001e208ecebbcbb4117c3
shirim <shirim@il.ibm.com>
2024-09-17 11:34:28 +0300

    refactoring, avoid dup code

--- pkg/commonvpc/explain_test_functionality.go
+++ pkg/commonvpc/explain_test_functionality.go
@@ -7,10 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package commonvpc
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 
 	"github.com/np-guard/models/pkg/netp"
 	"github.com/np-guard/vpc-network-config-analyzer/pkg/vpcmodel"
@@ -37,27 +34,11 @@ const explainOut = "explain_out"
 func (tt *VpcExplainTest) TestSingleExplain(t *testing.T, mode testMode, rc ResourcesContainer, testName string) {
 	tt.Name = testName
 	tt.setMode(mode)
-	t.Run(tt.Name, func(t *testing.T) {
-		t.Parallel()
-		tt.runExplainSingleTest(t, rc)
-	})
-}
-
-func (tt *VpcExplainTest) runExplainSingleTest(t *testing.T, rc ResourcesContainer) {
-	// all tests in explain mode
-	tt.UseCases = []vpcmodel.OutputUseCase{vpcmodel.Explain}
-	// init test - set the input/output file names according to test name
-	tt.InitTest()
-
-	// get vpcConfigs obj from parsing + analyzing input config file
-	vpcConfigs := tt.GetVPCConfigs(t, tt.InputConfig, rc)
 	explanationArgs := vpcmodel.NewExplanationArgs(tt.ESrc, tt.EDst, string(tt.EProtocol),
 		tt.ESrcMinPort, tt.ESrcMaxPort, tt.EDstMinPort, tt.EDstMaxPort, tt.DetailExplain)
-
-	// generate actual output for all use cases specified for this test
-	err := tt.RunTestPerUseCase(t, vpcConfigs, vpcmodel.Explain, tt.Mode, explainOut, explanationArgs)
-	require.Equal(t, tt.ErrPerUseCase[vpcmodel.Explain], err, "comparing explain actual err to expected err")
-	for uc, outFile := range tt.ActualOutput {
-		fmt.Printf("explain test %s use-case %d - generated output file: %s\n", tt.Name, uc, outFile)
-	}
+	tt.UseCases = []vpcmodel.OutputUseCase{vpcmodel.Explain}
+	t.Run(tt.Name, func(t *testing.T) {
+		t.Parallel()
+		tt.runSingleCommonTest(t, explainOut, rc, explanationArgs)
+	})
 }
