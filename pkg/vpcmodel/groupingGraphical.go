7fd6f410ca3a73cd490de110e935809066c44436
shirim <shirim@il.ibm.com>
2024-10-06 11:23:44 +0300

    self CR fixed

--- pkg/vpcmodel/groupingGraphical.go
+++ pkg/vpcmodel/groupingGraphical.go
@@ -10,7 +10,7 @@ import (
 	"github.com/np-guard/models/pkg/ipblock"
 )
 
-// for the html graphical representation. In the graph presentation, each node must have all relevant edges.
+// for the graphical (html, drawio, svg) representation. In the graph presentation, each node must have all relevant edges.
 // this is not the case in the textual presentation. E.g., a textual presentation may look like:
 // 142.0.64.0/17 ->vsi2
 // 142.0.0.0/16 -> vsi1
@@ -37,13 +37,14 @@ func getMapToGroupedExternalBlocks(grouped []*groupedConnLine) (eeToIpBlock map[
 }
 
 func addExternalEndpointToMap(ee EndpointElem, endpointsIPBlocks map[string]*ipblock.IPBlock) {
-	ipBlock := groupedExternalToIpBlock(ee)
-	if ipBlock == nil {
+	if !ee.IsExternal() {
 		return
 	}
-	if _, ok := endpointsIPBlocks[ee.Name()]; !ok {
-		endpointsIPBlocks[ee.Name()] = ipBlock
+	_, ok := endpointsIPBlocks[ee.Name()]
+	if ok { // no need to update twice; relevant if the same endpoint is in src and dst of different lines
+		return
 	}
+	endpointsIPBlocks[ee.Name()] = groupedExternalToIpBlock(ee)
 }
 
 func groupedExternalToIpBlock(ee EndpointElem) *ipblock.IPBlock {
@@ -63,6 +64,9 @@ func findContainEndpointMap(endpointsIPBlocks map[string]*ipblock.IPBlock) (cont
 	for containedEP, containedIP := range endpointsIPBlocks {
 		containingEPs := []string{}
 		for containingEP, containingIP := range endpointsIPBlocks {
+			if containingEP == containedEP {
+				continue
+			}
 			if containedIP.ContainedIn(containingIP) {
 				containingEPs = append(containingEPs, containingEP)
 			}
@@ -74,41 +78,13 @@ func findContainEndpointMap(endpointsIPBlocks map[string]*ipblock.IPBlock) (cont
 	return containedMap
 }
 
-// give the above containedMap adds edges of containing endpoints
+// given the above containedMap adds edges of containing endpoints
 func (g *GroupConnLines) addEdgesOfContainingEPs(containedMap map[string][]string) {
-	endpointToLines := g.getEndpointToLines()
+	endpointToLines := g.getEndpointToLines() // auxiliary map between each endpoint element to lines it participates in
+	// (as src or dst)
 	for _, toAddEdgesLine := range g.GroupedLines {
 		g.addEdgesToLine(toAddEdgesLine, endpointToLines, containedMap, true)
 		g.addEdgesToLine(toAddEdgesLine, endpointToLines, containedMap, false)
-	}
-}
-
-func (g *GroupConnLines) addEdgesToLine(line *groupedConnLine, endpointToLines map[string][]*groupedConnLine,
-	containedMap map[string][]string, src bool) {
-	nameToEndpointElem := map[string]EndpointElem{}
-	for _, line := range g.GroupedLines {
-		// there could be rewriting with identical values; not an issue complexity wise, and keeps the code simpler
-		nameToEndpointElem[line.Src.Name()] = line.Src
-		nameToEndpointElem[line.Dst.Name()] = line.Dst
-	}
-	var addToNodeName string
-	if src {
-		addToNodeName = line.Src.Name()
-	} else {
-		addToNodeName = line.Dst.Name()
-	}
-	for _, containedEndpoint := range containedMap[addToNodeName] {
-		for _, toAddLine := range endpointToLines[containedEndpoint] {
-			// adding edges; the other end of the edges will always be internal, since "this" edge is not internal
-			switch {
-			case src && toAddLine.Src.Name() == addToNodeName:
-				g.GroupedLines = append(g.GroupedLines, &groupedConnLine{Src: nameToEndpointElem[addToNodeName],
-					Dst: toAddLine.Dst, CommonProperties: toAddLine.CommonProperties})
-			case !src && toAddLine.Dst.Name() == addToNodeName:
-				g.GroupedLines = append(g.GroupedLines, &groupedConnLine{Src: toAddLine.Src,
-					Dst: nameToEndpointElem[addToNodeName], CommonProperties: toAddLine.CommonProperties})
-			}
-		}
 	}
 }
 
@@ -133,4 +109,35 @@ func addLineToMap(endpointToLines map[string][]*groupedConnLine, line *groupedCo
 		endpointToLines[name] = []*groupedConnLine{}
 	}
 	endpointToLines[name] = append(endpointToLines[name], line)
+}
+
+func (g *GroupConnLines) addEdgesToLine(line *groupedConnLine, endpointToLines map[string][]*groupedConnLine,
+	containedMap map[string][]string, src bool) {
+	nameToEndpointElem := map[string]EndpointElem{}
+	for _, line := range g.GroupedLines {
+		// there could be rewriting with identical values; not an issue complexity wise, not checking this keeps the code simpler
+		nameToEndpointElem[line.Src.Name()] = line.Src
+		nameToEndpointElem[line.Dst.Name()] = line.Dst
+	}
+	var addToNodeName string
+	if src {
+		addToNodeName = line.Src.Name()
+	} else {
+		addToNodeName = line.Dst.Name()
+	}
+	for _, containedEndpoint := range containedMap[addToNodeName] {
+		for _, toAddLine := range endpointToLines[containedEndpoint] {
+			// adding edges - namely, lines in grouping. "This" end of the edge is external (by design) and the "other"
+			// end of the edges will always be internal, since "this" edge is not internal.
+			// Grouping per internal endpoints is done (if requested) after this point
+			switch {
+			case src && toAddLine.Src.Name() == addToNodeName:
+				g.GroupedLines = append(g.GroupedLines, &groupedConnLine{Src: nameToEndpointElem[addToNodeName],
+					Dst: toAddLine.Dst, CommonProperties: toAddLine.CommonProperties})
+			case !src && toAddLine.Dst.Name() == addToNodeName:
+				g.GroupedLines = append(g.GroupedLines, &groupedConnLine{Src: toAddLine.Src,
+					Dst: nameToEndpointElem[addToNodeName], CommonProperties: toAddLine.CommonProperties})
+			}
+		}
+	}
 }
