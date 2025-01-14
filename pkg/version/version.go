c49e849c2a6fab17802aa265d17037be2097c625
Ziv Nevo <nevo@il.ibm.com>
2025-01-14 09:17:12 +0200

    version v0.7.1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,4 +7,4 @@ SPDX-License-Identifier: Apache-2.0
 package version
 
 // versionCore is the core portion of the vpc-network-config-analyzer CLI version per Semantic Versioning 2.0.0
-const VersionCore = "0.7.0"
+const VersionCore = "0.7.1"
